e358e71d719199da9c7677196d559475816141e1
Pedro Pinto <ppinto@ipg.pt>
2020-12-12 15:03:32 +0000

    first comit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/',function(req,res){
+    res.send("Site de Tecnologia");
+});
+
+app.get('/portateis',function(req,res){
+    res.send("Categoria de Portateis");
+});
+
+app.get('/smartphones',function(req,res){
+    res.send("Categoria de Smartphones");
+}); 
+
+app.get('/tablets',function(req,res){
+    res.send("Categoria de Tablets");
+});
+
+app.listen(3333,function(){
+    console.log("Servidor ativo no porto 3333");
+});
